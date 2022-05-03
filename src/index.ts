7b7acdaa130ca1ee66d49578793b4a26d067b566
javierch2022 <103612867+javierch2022@users.noreply.github.com>
2022-05-03 00:37:36 -0300

    final

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // desarrolle un alogaritmo que, de acuerdo a la altura de una persona, decida si puede entrar a un juego en eun parque de diversiones
 // para poder subirse a la montaña rusa la persona debe medir 1.30mts o mas
-//let dato = document.getElementById("dato");
+
 btnAltura.addEventListener("click", () => {
   console.log();
 
